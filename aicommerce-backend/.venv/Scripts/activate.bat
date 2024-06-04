0c7c608c371d7c51e036ad7db66c452edba6252f
Diego Drutman <diegod@logitec.com.ar>
2024-06-04 16:39:59 -0300

    producto: htmls

--- aicommerce-backend/.venv/Scripts/activate.bat
+++ aicommerce-backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Operador\AiCommerce\.venv
+set VIRTUAL_ENV=C:\Users\Operador\ai-commerce\aicommerce-backend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
